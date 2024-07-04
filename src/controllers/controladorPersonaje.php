6e52c8638deab8e1c85a3fb426a7d6cf54527462
yisus306 <yisus.lu306@gmail.com>
2024-07-03 19:28:59 -0600

    feat(characer-controller): creando conrolador personaje

---
+++ src/controllers/controladorPersonaje.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../database/conexion.php';
+include_once '../models/personaje.php';
+
+$modeloPersonaje = new Personaje($con);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nombre_personaje'])) {
+    $nombre = $_POST['nombre_personaje'];
+    $personaje = $modeloPersonaje->obtenerPersonalePorNombre($nombre);
+
+    // Almacenar en una cookie
+    setcookie("ultima_busqueda", json_encode($personaje), time() + (86400 * 30), "/");
+} else {
+    $personajes = $characterModel->obtenerPersonajes();
+}
+?>
