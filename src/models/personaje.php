42c66bde2cfc06dc7f2ab72da6169fb525ac1c05
yisus306 <yisus.lu306@gmail.com>
2024-07-03 18:34:11 -0600

    feat(character-model): agregando funcionalidades para obtener personaje por nombre y obtener todos los personajes

---
+++ src/models/personaje.php
@@ -0,0 +1,27 @@
+<?php
+
+class Personaje {
+    private $con;
+
+    public function _construct($con){
+        $this->con = $con;
+    }
+
+    public function obtenerPersonajePorNombre($nombre){
+        $sql = "CALL obtenerPersonajePorNombre(?)";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("s", $nombre);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    public function obtenerPersonajes(){
+        $sql = "SELECT * FROM personajes;";
+        $result = $this->con->query($sql);
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+
+}
+?>
