53fbc9072476fc63d2bd067485eb158c284311ba
yisus306 <yisus.lu306@gmail.com>
2024-07-05 15:34:51 -0600

    chore(sql): agregando la creacion de tablas

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,3 +1,17 @@
+CREATE TABLE personajes (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nombre VARCHAR(100) NOT NULL,
+            alias VARCHAR(100),
+            especie VARCHAR(100),
+            id_aparicion INT NOT NULL,
+            CONSTRAINT fk_id_aparicion FOREIGN KEY (id_aparicion) REFERENCES apariciones(id)
+        );
+
+CREATE TABLE apariciones (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo_comic VARCHAR(100) NOT NULL,
+        );
+        
 DELIMITER //
 
 CREATE PROCEDURE obtenerPersonajePorNombre (IN nombre_personaje VARCHAR(100))
